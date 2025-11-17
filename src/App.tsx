50eee68bb2b22063b257bcd38be7f5d3224cf277
Apple <apple@Apples-MacBook-Air.local>
2025-11-17 15:52:03 +0100

    Removed the Theme Context File

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -6,7 +5,6 @@
  * @format
  */
 
-import React from 'react';
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 import {
@@ -14,30 +12,20 @@ import {
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
 
-// Import the Theme Provider component
-import { AppThemeProvider } from '../src/theme/ThemeContext';
-
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    // The AppThemeProvider must wrap all components that need access to the theme,
-    // so we place it directly under SafeAreaProvider.
     <SafeAreaProvider>
-      <AppThemeProvider>
-        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        <AppContent />
-      </AppThemeProvider>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <AppContent />
     </SafeAreaProvider>
   );
 }
 
-// AppContent remains responsible for consuming the safe areas and rendering the main UI.
 function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
 
-  // NOTE: This NewAppScreen component will be replaced by your Auth/Root Navigator
-  // in the next sprint as we start building Nora's specific screens.
   return (
     <View style={styles.container}>
       <NewAppScreen
