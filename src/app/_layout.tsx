3a0b65e7b5b34d2231ef5e354b14bb162b762ad1
Asamu Caleb <116659689+Asamu22@users.noreply.github.com>
2025-11-19 03:15:35 -0800

    splash-screen

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -9,11 +9,11 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   // Load HankenGrotesk fonts
   const [fontsLoaded, fontsError] = useFonts({
-    "HankenGrotesk-Regular": require("@/src/app/assets/fonts/HankenGrotesk-Regular.ttf"),
-    "HankenGrotesk-Medium": require("@/src/app/assets/fonts/HankenGrotesk-Medium.ttf"),
-    "HankenGrotesk-SemiBold": require("@/src/app/assets/fonts/HankenGrotesk-SemiBold.ttf"),
-    "HankenGrotesk-Bold": require("@/src/app/assets/fonts/HankenGrotesk-Bold.ttf"),
-    "HankenGrotesk-ExtraBold": require("@/src/app/assets/fonts/HankenGrotesk-ExtraBold.ttf"),
+    "HankenGrotesk-Regular": require("@/assets/fonts/HankenGrotesk-Regular.ttf"),
+    "HankenGrotesk-Medium": require("@/assets/fonts/HankenGrotesk-Medium.ttf"),
+    "HankenGrotesk-SemiBold": require("@/assets/fonts/HankenGrotesk-SemiBold.ttf"),
+    "HankenGrotesk-Bold": require("@/assets/fonts/HankenGrotesk-Bold.ttf"),
+    "HankenGrotesk-ExtraBold": require("@/assets/fonts/HankenGrotesk-ExtraBold.ttf"),
   });
 
   useEffect(() => {
