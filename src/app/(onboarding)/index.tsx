9a85cb7ebe07817dae0167d19b99579fd11455ff
Asamu Caleb <116659689+Asamu22@users.noreply.github.com>
2025-11-18 16:45:09 -0800

    feat: splash screen

--- src/app/(onboarding)/index.tsx
+++ src/app/(onboarding)/index.tsx
@@ -1,15 +1,42 @@
-import { Text, View } from "react-native";
+import { colors, fontFamilies, typography } from "@/src/core/styles";
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
 
-export default function Index() {
+const LOGO_WIDTH = 120;
+const LOGO_HEIGHT = 172;
+const BRAND_FONT_SIZE = typography.heading1.fontSize;
+
+export default function OnboardingSplash() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Image
+        source={require("@/src/core/assets/fonts/images/logo.png")}
+        style={styles.logo}
+        resizeMode="contain"
+      />
+      <Text style={styles.brandName}>NORA</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.backgroundMain,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 24,
+  },
+  logo: {
+    width: LOGO_WIDTH,
+    height: LOGO_HEIGHT,
+  },
+  brandName: {
+    fontFamily: fontFamilies.extraBold,
+    fontSize: BRAND_FONT_SIZE,
+    lineHeight: BRAND_FONT_SIZE,
+    color: colors.primary,
+    letterSpacing: BRAND_FONT_SIZE * 0.1,
+    textAlign: "center",
+  },
+});
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,29 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, fontsError] = useFonts({
+    "HankenGrotesk-Regular": require("@/src/core/assets/fonts/HankenGrotesk-Regular.ttf"),
+    "HankenGrotesk-Medium": require("@/src/core/assets/fonts/HankenGrotesk-Medium.ttf"),
+    "HankenGrotesk-SemiBold": require("@/src/core/assets/fonts/HankenGrotesk-SemiBold.ttf"),
+    "HankenGrotesk-Bold": require("@/src/core/assets/fonts/HankenGrotesk-Bold.ttf"),
+    "HankenGrotesk-ExtraBold": require("@/src/core/assets/fonts/HankenGrotesk-ExtraBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontsError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontsError]);
+
+  if (!fontsLoaded && !fontsError) {
+    return null;
+  }
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(onboarding)" />
+    </Stack>
+  );
 }
